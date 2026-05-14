34a0a34970864f79457a4648bcd23bfb8d1406ec
bradc <brad.cocar@gmail.com>
2026-05-14 13:39:18 -0700

    Create JSON parser to load and validate targets into memory

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
+	"github.com/bradapc/distributed_health_monitor.git/internal/config"
+)
+
+func main() {
+	targets, err := config.LoadTargets("configs/targets.json")
+	if err != nil {
+		log.Fatalf("error: could not read target json: ", err)
+	}
+	fmt.Printf("Monitoring %d targets from file configs/targets.json\n", len(targets))
+	for i, tar := range targets {
+		fmt.Printf("%d: %s\n", i, tar.URL)
+	}
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,3 +1,67 @@
 package config
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+)
+
 //JSON parsing and config watcher
+
+type rawTarget struct {
+	URL      string `json:"url"`
+	Interval string `json:"interval"`
+	Timeout  string `json:"timeout"`
+}
+
+type MonitorTarget struct {
+	URL      string
+	Interval time.Duration
+	Timeout  time.Duration
+}
+
+func LoadTargets(filename string) ([]MonitorTarget, error) {
+	dataTargets, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("error loading targets from json file: %w", err)
+	}
+	var rawTargets []rawTarget
+	err = json.Unmarshal(dataTargets, &rawTargets)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshaling json targets: %w", err)
+	}
+	convertedTargets := make([]MonitorTarget, 0, len(rawTargets))
+	for _, t := range rawTargets {
+		ct, err := validateTarget(t)
+		if err != nil {
+			return nil, err
+		}
+		convertedTargets = append(convertedTargets, ct)
+	}
+	return convertedTargets, nil
+}
+
+func validateTarget(target rawTarget) (MonitorTarget, error) {
+	if target.URL == "" {
+		return MonitorTarget{}, fmt.Errorf("invalid url on target: %s", target.URL)
+	}
+	inter, err := time.ParseDuration(target.Interval)
+	if err != nil {
+		return MonitorTarget{}, fmt.Errorf("target %s: invalid interval: %w", target.URL, err)
+	}
+	to, err := time.ParseDuration(target.Timeout)
+	if err != nil {
+		return MonitorTarget{}, fmt.Errorf("target %s: invalid timeout: %w", target.URL, err)
+	}
+	if to > inter {
+		return MonitorTarget{}, fmt.Errorf("target %s: timeout cannot be greater than interval", target.URL)
+	}
+
+	var monitorTarget MonitorTarget = MonitorTarget{
+		URL:      target.URL,
+		Interval: inter,
+		Timeout:  to,
+	}
+	return monitorTarget, nil
+}
