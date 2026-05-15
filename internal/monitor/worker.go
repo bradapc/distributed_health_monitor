58616be04f467122a47c38859f025b621fe0398b
bradc <brad.cocar@gmail.com>
2026-05-14 21:12:44 -0700

    Add finite state machine for working timeout and cooldown on request failure

--- internal/monitor/worker.go
+++ internal/monitor/worker.go
@@ -11,15 +11,29 @@ import (
 
 //Per-URL loop logic and state machine
 
+type State int
+
+const (
+	Open State = iota
+	HalfOpen
+	Closed
+)
+
+const FailureThreshold int = 3
+const TimeoutThreshold int = 10
+
 type Worker struct {
-	Target       config.MonitorTarget
-	Client       *http.Client
+	Target config.MonitorTarget
+	Client *http.Client
+
+	CurrentState State
 	FailureCount int
 	LastFailure  time.Time
 }
 
 func (w *Worker) RunWorker(ctx context.Context) {
 	ticker := time.NewTicker(w.Target.Interval)
+	w.CurrentState = Closed
 	defer ticker.Stop()
 	for {
 		select {
@@ -32,6 +46,16 @@ func (w *Worker) RunWorker(ctx context.Context) {
 }
 
 func (w *Worker) performCheck(ctx context.Context) {
+	if w.CurrentState == Open {
+		timeElapsed := time.Since(w.LastFailure)
+		if timeElapsed.Seconds() >= float64(TimeoutThreshold) {
+			fmt.Printf("%s cooldown finished, retrying...\n", w.Target.URL)
+			w.CurrentState = HalfOpen
+		} else {
+			return
+		}
+	}
+
 	reqCtx, cancel := context.WithTimeout(ctx, w.Target.Timeout)
 	defer cancel()
 
@@ -40,17 +64,42 @@ func (w *Worker) performCheck(ctx context.Context) {
 		return
 	}
 
-	//start := time.Now()
+	start := time.Now()
 	resp, err := w.Client.Do(req)
-	//latency := time.Since(start)
+	latency := time.Since(start)
 
 	if err != nil {
-		//HandleError(err)
+		fmt.Println(err.Error())
+		w.HandleFailure()
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 500 && resp.StatusCode < 600 {
+		fmt.Printf("[%s] soft failure, status code in 5xx range\n", w.Target.URL)
+		w.HandleFailure()
 		return
 	}
 
-	defer resp.Body.Close()
-	fmt.Printf("[%s] Status: %d\n", w.Target.URL, resp.StatusCode)
+	if w.CurrentState == HalfOpen {
+		w.CurrentState = Closed
+	}
+
+	w.FailureCount = 0
+
+	fmt.Printf("[%s] Status: %d in time %s\n", w.Target.URL, resp.StatusCode, latency)
 
 	//LogResult(resp.StatusCode, latency)
+}
+
+func (w *Worker) HandleFailure() {
+	w.FailureCount++
+	if w.CurrentState == HalfOpen {
+		w.CurrentState = Open
+		fmt.Printf("[%s] retry failed, cooling down again...\n", w.Target.URL)
+	} else if w.FailureCount == FailureThreshold {
+		w.CurrentState = Open
+		fmt.Printf("[%s] timed out 3x, cooling down...\n", w.Target.URL)
+	}
+	w.LastFailure = time.Now()
 }
